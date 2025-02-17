6d94a6c7e75ae2d72cc19064c213509f4f95fb5f
Jonathon Broughton <jonathon@stardotbmp.com>
2025-02-17 23:43:18 +0000

    Update predicate method mappings
    Switched from string references to actual method names in the predicate mapping. This improves clarity and reduces errors by directly linking predicates to their corresponding methods in PropertyRules. Adjusted some predicate phrases for consistency as well.

--- src/predicates.py
+++ src/predicates.py
@@ -1,18 +1,19 @@
 """Configuration module defining mappings between spreadsheet predicates and rule methods."""
 
+from src.rules import PropertyRules
+
 # Mapping of input predicates to the corresponding methods in PropertyRules
 PREDICATE_METHOD_MAP = {
-    "exists": "has_parameter",
-    "matches": "is_parameter_value",
-    "greater than": "is_parameter_value_greater_than",
-    "less than": "is_parameter_value_less_than",
-    "in range": "is_parameter_value_in_range",
-    "in list": "is_parameter_value_in_list",
-    "equals": "is_equal_value",
-    "identical": "is_identical_value",
-    "not equal": "is_not_equal_value",
-    "not identical": "is_not_identical_value",
-    "true": "is_parameter_value_true",
-    "false": "is_parameter_value_false",
-    "is like": "is_parameter_value_like",
+    "exists": PropertyRules.has_parameter.__name__,
+    "matches": PropertyRules.is_parameter_value.__name__,
+    "greater than": PropertyRules.is_parameter_value_greater_than.__name__,
+    "less than": PropertyRules.is_parameter_value_less_than.__name__,
+    "in range": PropertyRules.is_parameter_value_in_range.__name__,
+    "in list": PropertyRules.is_parameter_value_in_list.__name__,
+    "equal to": PropertyRules.is_equal_value.__name__,
+    "is true": PropertyRules.is_parameter_value_true.__name__,
+    "is false": PropertyRules.is_parameter_value_false.__name__,
+    "is like": PropertyRules.is_parameter_value_like.__name__,
+    "identical to": PropertyRules.is_identical_value.__name__,
+    "not equal": PropertyRules.is_not_equal_value.__name__,
 }
